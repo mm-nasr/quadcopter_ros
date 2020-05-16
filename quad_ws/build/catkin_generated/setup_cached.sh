56abcd67b9034a4946f7b2c6fa05d8f3c11f3e12
mmmnasrm <mmmnasrm@uwaterloo.ca>
2020-05-16 01:46:06 -0400

    Initialized catkin workspace

---
+++ quad_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/nasr/robotics/quadcopter_ros/quad_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/nasr/robotics/quadcopter_ros/quad_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/nasr/robotics/quadcopter_ros/quad_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/nasr/robotics/quadcopter_ros/quad_ws/src:$ROS_PACKAGE_PATH"
